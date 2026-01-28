f9a4c59933224c92c1e5d83b8e28f3f3ae90dfd9
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2026-01-28 18:28:54 -0500

    style: apply cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use anyhow::{Result};
-use clap::Parser;
-use std::str::FromStr;
+use anyhow::Result;
 use cidr::{Ipv4Cidr, Ipv6Cidr};
-use tracing::{info};
-use syslog_tracing::{Syslog};
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::EnvFilter;
+use clap::Parser;
 use daemonize::Daemonize;
 use hostwatch::Args;
 use hostwatch::HostWatch;
-
-
+use std::str::FromStr;
+use syslog_tracing::Syslog;
+use tracing::info;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
 
 fn main() -> Result<()> {
     let args = Args::parse();
@@ -36,21 +34,29 @@ fn main() -> Result<()> {
         let identity = std::ffi::CStr::from_bytes_with_nul(b"hostwatch\0")?;
         let (options, facility) = Default::default();
         let syslog = Syslog::new(identity, options, facility).unwrap();
-        Some(tracing_subscriber::fmt::layer()
-            .pretty()
-            .with_ansi(false)
-            .with_line_number(false)
-            .with_file(false)
-            .with_writer(syslog))
+        Some(
+            tracing_subscriber::fmt::layer()
+                .pretty()
+                .with_ansi(false)
+                .with_line_number(false)
+                .with_file(false)
+                .with_writer(syslog),
+        )
     } else {
         None
     };
 
-    let filter = EnvFilter::from_default_env()
-        .add_directive(format!("hostwatch={}", level).parse()?);
-    tracing_subscriber::registry().with(filter).with(stdout_layer).with(syslog_layer_opt).init();
+    let filter =
+        EnvFilter::from_default_env().add_directive(format!("hostwatch={}", level).parse()?);
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(stdout_layer)
+        .with(syslog_layer_opt)
+        .init();
 
-    if !args.interface.contains(&"any".to_string()) && !args.filename.clone().unwrap_or("".to_string()).is_empty() {
+    if !args.interface.contains(&"any".to_string())
+        && !args.filename.clone().unwrap_or("".to_string()).is_empty()
+    {
         println!("File [-F] and interface [-i] modes can not be combined");
         std::process::exit(1);
     }
@@ -67,8 +73,8 @@ fn main() -> Result<()> {
             Ok(_cidr) => true,
             Err(_e) => match Ipv6Cidr::from_str(network) {
                 Ok(_cidr) => true,
-                Err(_e) => false
-            }
+                Err(_e) => false,
+            },
         };
         if !is_valid {
             println!("Network {} not valid", network);
@@ -87,9 +93,7 @@ fn main() -> Result<()> {
             .pid_file(args.clone().pid_file.as_str())
             .chown_pid_file(args.chown_pid_file)
             .working_directory(args.clone().working_directory.unwrap().as_str())
-            .privileged_action(|| {
-                hostwatch.expect("Unable to initialize captures").init()
-            });
+            .privileged_action(|| hostwatch.expect("Unable to initialize captures").init());
 
         if args.clone().user.is_some() {
             daemonize = daemonize.user(args.clone().user.unwrap().as_str());
@@ -99,9 +103,7 @@ fn main() -> Result<()> {
         }
 
         match daemonize.start() {
-            Ok(child) => {
-                child.run().expect("Failed to execute captures")
-            },
+            Ok(child) => child.run().expect("Failed to execute captures"),
             Err(e) => eprintln!("Error, {}", e),
         }
     }
